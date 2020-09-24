a0c62f2dd7b9651f51f94d84e68050aa547801b5
T. Bunratta <tharathorn.bu@ku.th>
2020-09-24 14:09:36 +0700

    Add a code-based configuration variant

--- src/main/java/th/ac/ku/atm/Main.java
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -1,11 +1,11 @@
 package th.ac.ku.atm;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(ATMConfig.class);
         AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
     }
